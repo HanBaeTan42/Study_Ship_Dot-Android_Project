039a4d396d5a6adfb10905b6cefe4ff8163c2935
onenewkong <hub1375@kpu.ac.kr>
2023-01-23 01:48:22 +0900

    fix/#8/Create FindId_page design

--- app/src/main/java/com/hb/study_ship_dot/fragment/FindIdFragment.kt
+++ app/src/main/java/com/hb/study_ship_dot/fragment/FindIdFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [BlankFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BlankFragment : Fragment() {
+class FindIdFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,7 +50,7 @@ class BlankFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BlankFragment().apply {
+            FindIdFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
