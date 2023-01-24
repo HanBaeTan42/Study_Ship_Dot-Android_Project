82d68fb3de865d8c4b3f40034ead80425e68d927
heebum99 <heebum9955@naver.com>
2023-01-24 14:02:27 +0900

    fix/#6/Modify page rename

--- app/src/main/java/com/hb/study_ship_dot/Fragment/ChkInfoPw.kt
+++ app/src/main/java/com/hb/study_ship_dot/Fragment/ChkInfoPw.kt
@@ -17,10 +17,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FindPwFragment.newInstance] factory method to
+ * Use the [ChkInfoPw.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FindPwFragment : Fragment() {
+class ChkInfoPw : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -39,7 +39,7 @@ class FindPwFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_pw, container, false)
+        return inflater.inflate(R.layout.fragment_chk_info_pw, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -64,7 +64,7 @@ class FindPwFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FindPwFragment().apply {
+            ChkInfoPw().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
