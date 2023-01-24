1ee8fc197aaf72f0a8a717b9a5581b78895f8885
Hj1218 <wjdguswl2341@naver.com>
2023-01-24 14:15:22 +0900

    1

--- app/src/main/java/com/hb/study_ship_dot/fragment/JoinFragment.kt
+++ app/src/main/java/com/hb/study_ship_dot/fragment/JoinFragment.kt
@@ -9,30 +9,15 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.hb.study_ship_dot.R
 import kotlinx.android.synthetic.main.fragment_join.*
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [JoinFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class JoinFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setContentView(R.layout.fragment_join)
+
+        join.
     }
 
     override fun onCreateView(
@@ -55,25 +40,5 @@ class JoinFragment : Fragment() {
         btn_join_join.setOnClickListener { //회원가입 버튼을 눌렀을 때 로그인 페이지로 이동
             navController.navigate(R.id.loginFragment)
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment JoinFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            JoinFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
