c543e6db5f61225b335d517a28f4d8979fec5ed5
heebum kim <108645814+heebum99@users.noreply.github.com>
2023-01-26 13:41:08 +0900

    Revert "Feat/#20/login"

--- app/src/main/java/com/hb/study_ship_dot/MainActivity.kt
+++ app/src/main/java/com/hb/study_ship_dot/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.hb.study_ship_dot
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        navController = findNavController(R.id.nav_host_fragment)
     }
 }
