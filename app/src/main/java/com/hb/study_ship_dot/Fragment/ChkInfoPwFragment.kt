8437d97dddbf5adf1b651863dca156f5047410ea
heeyeon <dlgmldus3289@naver.com>
2023-01-26 15:45:06 +0900

    test/#19/ChkInfo_Pw function

--- app/src/main/java/com/hb/study_ship_dot/Fragment/ChkInfoPwFragment.kt
+++ app/src/main/java/com/hb/study_ship_dot/Fragment/ChkInfoPwFragment.kt
@@ -1,77 +1,32 @@
 package com.hb.study_ship_dot.fragment
 
-import android.app.AlertDialog
-import android.app.Application
-import android.content.Context
-import android.content.DialogInterface
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.hb.study_ship_dot.R
 import kotlinx.android.synthetic.main.fragment_chk_info_pw.*
 
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 class ChkInfoPwFragment : Fragment() {
-    val TAG:String="ChkInfoPwFragment"
-    lateinit var navController: NavController
 
+    private var param1: String? = null
+    private var param2: String? = null
+
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //비밀번호 변경 버튼
-        btn_changePw_chkInfoPw.setOnClickListener {
-            //editText로부터 입력된 값을 받아옴
-            val id=InputId_chkInfoPw.text.toString()
-            val name=inputName_chkInfoPw.text.toString()
-            val pn=inputPn_chkInfoPw.text.toString()
-
-            //쉐어드로부터 저장된 id,name,pn 가져오기
-            //val sharedPreference=getSharedPreferences("Info",Context.MODE_PRIVATE)
-            val saveId=MyApplication.prefs.getString("id","")
-            val saveName=MyApplication.prefs.getString("name","")
-            val savePn=MyApplication.prefs.getString("pn","")
-            //사용자가 입력한 값과 쉐어드에서 불러온 값 비교
-            if(id == saveId && name==saveName && pn==savePn){
-                //회원정보 일치 다이얼로그
-                dialog("success")
-            }
-            else {
-                //회원정보 일치X 다이얼로그
-                dialog("fail")
-            }
-
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
         }
-
     }
-
-    fun dialog(type: String){
-
-        val dialog=AlertDialog.Builder(context)
-        if (type.equals("success")){
-            dialog.setTitle("회원정보 일치")
-            dialog.setMessage("비밀번호 변경 페이지로 넘어갑니다.")
-        }
-        else if(type.equals("fail")){
-            dialog.setTitle("회원정보 불일치")
-            dialog.setMessage("회원정보를 다시 입력해주세요.")
-        }
-        var dialog_listener=object: DialogInterface.OnClickListener{
-            override fun onClick(dialog: DialogInterface?, which: Int) {
-                when(which){
-                    DialogInterface.BUTTON_POSITIVE -> Log.d(TAG,"")
-                }
-            }
-        }
-        dialog.setPositiveButton("확인",dialog_listener)
-        dialog.show()
-    }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -86,33 +41,55 @@ class ChkInfoPwFragment : Fragment() {
 
         navController = Navigation.findNavController(view)
 
-        btn_changePw_chkInfoPw.setOnClickListener { //비밀번호 변경 버튼을 눌렀을 때 비밀번호 변경 페이지로 이동
-            navController.navigate(R.id.changePwFragment)
+        btn_changePw_chkInfoPw.setOnClickListener { //비밀번호 변경 버튼을 눌렀을 때
+
+            //editText로부터 입력된 값을 받아옴
+            var id=InputId_chkInfoPw.text.toString()
+            var name=inputName_chkInfoPw.text.toString()
+            var pn=inputPn_chkInfoPw.text.toString()
+
+            //파일들
+            val Index_file = context?.getSharedPreferences("Index_file", 0)
+            val Name_file = context?.getSharedPreferences("Name_file", 0)
+            val Pn_file = context?.getSharedPreferences("Pn_file", 0)
+            val Id_file = context?.getSharedPreferences("Id_file", 0)
+
+            //인덱스
+            val index=Index_file?.getInt("index",0)
+
+            //쉐어드로부터 저장된 id,name,pn 가져오기
+            val saveId=Id_file?.getString("$index","")
+            val saveName=Name_file?.getString("$index","")
+            val savePn=Pn_file?.getString("$index","")
+
+            //사용자가 입력한 값과 쉐어드에서 불러온 값 비교
+            if(id == saveId && name==saveName && pn==savePn){
+                //회원정보 일치
+                Toast.makeText(context,"회원정보가 일치합니다.\n비밀번호 변경 페이지로 이동합니다.",Toast.LENGTH_SHORT)
+                //비밀번호 변경 페이지로 이동
+                navController.navigate(R.id.changePwFragment)
+            }
+            else {
+                //회원정보 일치X
+                Toast.makeText(context,"회원정보가 일치하지 않습니다.\n다시입력해주세요.",Toast.LENGTH_SHORT)
+            }
+
+
+
+
         }
+
     }
 
-
-}
-class MyApplication : Application() {
     companion object {
-        lateinit var prefs: PreferenceUtil
-    }
-
-    override fun onCreate() {
-        prefs = PreferenceUtil(applicationContext)
-        super.onCreate()
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ChkInfoPwFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
-class PreferenceUtil(context: Context) {
-    private val prefs: SharedPreferences =
-        context.getSharedPreferences("Info", Context.MODE_PRIVATE)
 
-    fun getString(key: String, defValue: String): String {
-        return prefs.getString(key, defValue).toString()
-    }
-
-    fun setString(key: String, str: String) {
-        prefs.edit().putString(key, str).apply()
-    }
-
-}
